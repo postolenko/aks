a779c4992d4c2d09489238b7c1a39d64fa347941
postolenko <denis.postolenko@gmail.com>
2020-02-11 19:38:58 +0200

    EC

--- js/scripts.js
+++ js/scripts.js
@@ -100,6 +100,24 @@ function getPopupSlider() {
   }
 }
 
+function detectIEEdge() {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+       return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+    return false;
+}
+
 var w = window,
 d = document,
 e = d.documentElement,
@@ -130,6 +148,9 @@ $(document).ready(function() {
     getSertificatesSlider();
     getTestimonialSlider();
     getPopupSlider();
+    if(detectIEEdge() != false) {
+      $("body").addClass("ie");
+    }
 
     if( $(".promo_slider").length > 0 ) {
         var indexActive;
